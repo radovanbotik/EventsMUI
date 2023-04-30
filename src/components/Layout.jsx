bcf7cdd484c3c1ee4393bcdae5ea825f3449ae5e
radovanbotik <radovanbotik@gmail.com>
2023-04-30 18:24:24 +0200

    ''

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,13 @@
 import CssBaseline from "@mui/material/CssBaseline";
-import { AppBar, Box, Typography, Toolbar, Drawer, IconButton, Menu, MenuItem } from "@mui/material";
-import { Event, People, AccountCircle, Menu as MenuIcon } from "@mui/icons-material";
+import { Box, Toolbar, Drawer } from "@mui/material";
+import { Event, People } from "@mui/icons-material";
 import { Outlet } from "react-router-dom";
 import { useState } from "react";
 import DrawerContent from "../features/DrawerContent";
-
-const drawerWidth = 240;
+import Bar from "../features/Bar";
 
 function ResponsiveDrawer(props) {
+  const drawerWidth = 240;
   const { window } = props;
   const [mobileOpen, setMobileOpen] = useState(false);
   const [auth, setAuth] = useState(true);
@@ -17,16 +17,9 @@ function ResponsiveDrawer(props) {
   const [formOpen, setFormOpen] = useState(false);
   const [editing, setEditing] = useState(false);
   const [currentEvent, setCurrentEvent] = useState("");
+
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
-  };
-
-  const handleMenu = event => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
   };
 
   const routes = [
@@ -39,60 +32,16 @@ function ResponsiveDrawer(props) {
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-        }}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-            Event Organizer Or Something
-          </Typography>
-          {auth && (
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-              </Menu>
-            </div>
-          )}
-        </Toolbar>
-      </AppBar>
+      <Bar
+        drawerWidth={drawerWidth}
+        mobileOpen={mobileOpen}
+        setMobileOpen={setMobileOpen}
+        auth={auth}
+        setAuth={setAuth}
+        anchorEl={anchorEl}
+        setAnchorEl={setAnchorEl}
+        handleDrawerToggle={handleDrawerToggle}
+      />
       <Box component="nav" sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }} aria-label="mailbox folders">
         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Drawer
---
+++ src/features/Bar.jsx
@@ -0,0 +1,74 @@
+import { AppBar, Toolbar, IconButton, Typography, Menu, MenuItem } from "@mui/material";
+import { AccountCircle, Menu as MenuIcon } from "@mui/icons-material";
+
+const Bar = ({ handleDrawerToggle, drawerWidth, mobileOpen, setMobileOpen, auth, setAuth, anchorEl, setAnchorEl }) => {
+  const handleMenu = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <AppBar
+      position="fixed"
+      sx={{
+        width: { sm: `calc(100% - ${drawerWidth}px)` },
+        ml: { sm: `${drawerWidth}px` },
+      }}
+    >
+      <Toolbar>
+        {/* Menu Icon */}
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
+          sx={{ mr: 2, display: { sm: "none" } }}
+        >
+          <MenuIcon />
+        </IconButton>
+        {/* AppTitle */}
+        <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+          Event Organizer Or Something
+        </Typography>
+        {/* Profile Menu */}
+        {auth && (
+          <div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+            </Menu>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Bar;
