7d9fec7274e71d951c525d40bf3a81ae09ba1b8d
radovanbotik <radovanbotik@gmail.com>
2023-04-30 19:09:42 +0200

    ''

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -4,6 +4,7 @@ import {
   IconButton,
   CardHeader,
   CardActions,
+  CardActionArea,
   CardMedia,
   CardContent,
   List,
@@ -17,6 +18,7 @@ import {
 import { MoreVert } from "@mui/icons-material";
 import { useDispatch } from "react-redux";
 import { deleteEvent } from "../store/slice";
+import { Link } from "react-router-dom";
 
 const EventCard = ({
   attendees,
@@ -41,44 +43,48 @@ const EventCard = ({
   console.log(formOpen);
   return (
     <Card>
-      <CardHeader
-        avatar={<Avatar aria-label="host" src={hostPhotoURL} /> || <Avatar />}
-        action={
-          <IconButton aria-label="options">
-            <MoreVert />
-          </IconButton>
-        }
-        title={title}
-        subheader={`by ${hostedBy || "anonymous"}`}
-      ></CardHeader>
-      <CardMedia
-        component="img"
-        image={
-          eventPhotoURL ||
-          "https://media.istockphoto.com/id/513822999/photo/garbage-bin-outdoors-overflowing-with-trash.jpg?s=1024x1024&w=is&k=20&c=UDDkN_RligWzBQlJKApvm4vPP-4if9eNJb_-8hm9rrc="
-        }
-        alt="event image"
-        height={300}
-      ></CardMedia>
-      <CardContent>
-        <List dense>
-          <ListItem>
-            <AvatarGroup>
-              {attendees?.map(attendee => {
-                return <Avatar key={attendee.id} alt="Guest" src={attendee.photoURL} sx={{ width: 24, height: 24 }} />;
-              })}
-            </AvatarGroup>
-            <ListItemText primary={`${attendees?.[0].name} and others`}></ListItemText>
-            <Typography>{city}</Typography>
-            <Divider>on</Divider>
-            <Typography>{date}</Typography>
-          </ListItem>
-          <Divider variant="inset" component="li" />
-          <ListItem>
-            <ListItemText primary={description} secondary={"2 minutes ago"} />
-          </ListItem>
-        </List>
-      </CardContent>
+      <CardActionArea component={Link} to={id}>
+        <CardHeader
+          avatar={<Avatar aria-label="host" src={hostPhotoURL} /> || <Avatar />}
+          action={
+            <IconButton aria-label="options">
+              <MoreVert />
+            </IconButton>
+          }
+          title={title}
+          subheader={`by ${hostedBy || "anonymous"}`}
+        ></CardHeader>
+        <CardMedia
+          component="img"
+          image={
+            eventPhotoURL ||
+            "https://media.istockphoto.com/id/513822999/photo/garbage-bin-outdoors-overflowing-with-trash.jpg?s=1024x1024&w=is&k=20&c=UDDkN_RligWzBQlJKApvm4vPP-4if9eNJb_-8hm9rrc="
+          }
+          alt="event image"
+          height={300}
+        ></CardMedia>
+        <CardContent>
+          <List dense>
+            <ListItem>
+              <AvatarGroup>
+                {attendees?.map(attendee => {
+                  return (
+                    <Avatar key={attendee.id} alt="Guest" src={attendee.photoURL} sx={{ width: 24, height: 24 }} />
+                  );
+                })}
+              </AvatarGroup>
+              <ListItemText primary={`${attendees?.[0].name} and others`}></ListItemText>
+              <Typography>{city}</Typography>
+              <Divider>on</Divider>
+              <Typography>{date}</Typography>
+            </ListItem>
+            <Divider variant="inset" component="li" />
+            <ListItem>
+              <ListItemText primary={description} secondary={"2 minutes ago"} />
+            </ListItem>
+          </List>
+        </CardContent>
+      </CardActionArea>
       <CardActions>
         <Button
           size="small"
