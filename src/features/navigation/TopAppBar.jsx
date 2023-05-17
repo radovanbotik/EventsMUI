c280ad33b4825cd411e9502f97b32a677d04e2e9
radovanbotik <radovanbotik@gmail.com>
2023-05-17 10:18:10 +0200

    test'

--- src/features/navigation/TopAppBar.jsx
+++ src/features/navigation/TopAppBar.jsx
@@ -47,6 +47,7 @@ const Bar = ({ handleDrawerToggle, drawerWidth, auth, anchorEl, setAnchorEl }) =
         >
           <MenuIcon />
         </IconButton>
+        <Button>hello</Button>
         {/* AppTitle */}
         <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
           Dashboard
