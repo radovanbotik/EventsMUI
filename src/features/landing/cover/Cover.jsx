7ae477c0ae53fa1b6395fbf1f9660f6ba6c81bd8
radovanbotik <radovanbotik@gmail.com>
2023-06-10 00:00:04 +0200

    carousel

--- src/features/landing/cover/Cover.jsx
+++ src/features/landing/cover/Cover.jsx
@@ -1,22 +1,45 @@
 import { Box } from "@mui/material";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Navigation, Pagination } from "swiper";
+import { Autoplay, Navigation, Pagination, EffectCreative } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
 import "swiper/css/pagination";
+import "swiper/css/effect-creative";
 
 import trnava from "../../../common/images/trnava.webp";
 import szeged from "../../../common/images/szeged.webp";
+import Slide from "./Slide";
+
+const cities = [
+  {
+    city: "trnava",
+    photo: trnava,
+  },
+  {
+    city: "szeged",
+    photo: szeged,
+  },
+];
 
 const Cover = () => {
   return (
     <Box sx={{ flex: 1, width: "100%", maxWidth: "100%", maxHeight: "100vh", minHeight: 0, minWidth: 0 }}>
       <Swiper
+        effect={"creative"}
+        creativeEffect={{
+          prev: {
+            shadow: true,
+            translate: [0, 0, -50],
+          },
+          next: {
+            translate: ["100%", 0, 0],
+          },
+        }}
         slidesPerView={1}
         onSlideChange={() => console.log("slide has changed")}
         onSwiper={() => console.log("swiper")}
-        speed={500}
+        speed={800}
         autoplay={{
           delay: 5000,
           disableOnInteraction: false,
@@ -27,15 +50,14 @@ const Cover = () => {
           clickable: "true",
           dynamicBullets: true,
         }}
-        modules={[Autoplay, Navigation, Pagination]}
+        modules={[Autoplay, Navigation, Pagination, EffectCreative]}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <img src={trnava} alt="trnava" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={szeged} alt="szeged" />
-        </SwiperSlide>
+        {cities.map((city) => (
+          <SwiperSlide key={city.city}>
+            <Slide {...city} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </Box>
   );
--- src/features/landing/cover/Slide.jsx
+++ src/features/landing/cover/Slide.jsx
@@ -1,8 +1,38 @@
-import React from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
-const Slide = () => {
-  return <Box></Box>;
+const Slide = ({ city, photo }) => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "100%",
+        position: "relative",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "end",
+      }}
+    >
+      <img
+        src={photo}
+        alt="trnava"
+        style={{ position: "absolute", width: "100%", height: "100%", display: "block", objectFit: "cover" }}
+      />
+      <Box
+        sx={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          display: "block",
+          objectFit: "cover",
+          zIndex: 2,
+        }}
+        className="gradient-overlay"
+      />
+      {/* <Typography style={{ zIndex: 3 }} variant="h1" sx={{ mb: 10 }} color={"white"} textTransform="uppercase">
+        {city}
+      </Typography> */}
+    </Box>
+  );
 };
 
 export default Slide;
